ae90d1e2f392eec8ee5e86e9a7bc976813c83406
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-05-22 10:09:11 +0900

    Update K번째수.js

--- Lv.1/K번째수.js
+++ Lv.1/K번째수.js
@@ -1,1 +1,17 @@
-// code
+// Limbi
+function solution(array, commands) {
+    let answer = [];
+    for (let i=0; i<commands.length; i++) {
+        const start = commands[i][0]-1;
+        const end = commands[i][1];
+        const k = commands[i][2];
+        
+        const arr = array.slice(start, end)
+        arr.sort(function(a, b) {
+            return a-b;
+        })
+        answer.push(arr[k-1]);
+        
+    }
+    return answer;
+}
