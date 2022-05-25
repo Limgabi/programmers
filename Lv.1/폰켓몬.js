15d55940b0bf9a74d1060a1ee2dadb4690bd0c46
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-05-25 20:12:36 +0900

    Update 폰켓몬.js

--- Lv.1/폰켓몬.js
+++ Lv.1/폰켓몬.js
@@ -1,1 +1,11 @@
-// code
+// Limbi
+function solution(nums) {
+    let answer = 0;
+    let set = new Set(nums);
+    
+    if (Math.floor(nums.length / 2) <= set.size) {
+        answer = Math.floor(nums.length / 2);
+    } else answer = set.size;
+    
+    return answer;
+}
