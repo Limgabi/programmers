8dc6844a63b37fe49cd6761ac7ac1dd795c016a5
suzinxix <72126893+suzinxix@users.noreply.github.com>
2022-05-26 23:51:52 +0900

    Update 완주하지 못한 선수.js

--- Lv.1/완주하지 못한 선수.js
+++ Lv.1/완주하지 못한 선수.js
@@ -1,1 +1,10 @@
-// code
+// Dea
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+    for(let i = 0; i < participant.length; i++){
+        if(participant[i] !== completion[i]){
+            return participant[i];
+        }
+    }
+}
