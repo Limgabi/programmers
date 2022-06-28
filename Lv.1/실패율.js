a4da12e6744ba88fb3d0da65b8a87b43effff0ca
suzinxix <72126893+suzinxix@users.noreply.github.com>
2022-06-28 23:45:54 +0900

    Update 실패율.js

--- Lv.1/실패율.js
+++ Lv.1/실패율.js
@@ -13,3 +13,19 @@ function solution(N, stages) {
     stage.map((i) => answer.push(i[0]));
     return answer;
 }
+
+
+// Dea
+function solution(N, stages) {
+    let failureRate = []; // 실패율
+    let reachedPlayer = stages.length; // 해당 스테이지에 도달한 플레이어 수
+  
+  for (let i = 1; i <= N; i += 1) {
+    let num = stages.filter(e => e === i).length; // 해당 스테이지 멈춰있는 플레이어 수
+    failureRate.push({ stageNum: i, rate: num / reachedPlayer });
+    reachedPlayer -= num;
+  }
+
+     failureRate.sort((a, b) => b.rate - a.rate);
+  return failureRate.map(obj => obj.stageNum); // 스테이지 번호로 되어있는 새로운 배열 생성
+}
