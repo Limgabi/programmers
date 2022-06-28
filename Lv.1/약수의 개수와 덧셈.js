24357dc0baa2d5f4d2130cf5b62514d6e99214e0
Ivoryeee <105477246+Ivoryeee@users.noreply.github.com>
2022-06-29 01:30:24 +0900

    Update 약수의 개수와 덧셈.js

--- Lv.1/약수의 개수와 덧셈.js
+++ Lv.1/약수의 개수와 덧셈.js
@@ -36,3 +36,20 @@ function solution(left, right) {
     }
     return sum;
 }
+
+
+//ivoryeee
+
+function solution(left, right) {
+    var answer = 0;
+    for(let i=left; i<=right; i++){
+        let array=[];
+        // i의 약수 찾기
+        for(let j = 0; j <= i; j++) {
+            if(i % j === 0) array.push(j) //j가 i의 약수라면 array에 j를 집어넣음 
+        }
+        array.length % 2 === 0 ? answer += i : answer -= i
+        //array가 짝수라면 answer에 i값을 더해주고, 홀수라면 빼줌 
+    }
+    return answer
+}
