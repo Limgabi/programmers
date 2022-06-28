616cd90a5392e33e515e8f802e2c53b47814a273
Ivoryeee <105477246+Ivoryeee@users.noreply.github.com>
2022-06-29 01:36:58 +0900

    Update 3진법 뒤집기.js

--- Lv.1/3진법 뒤집기.js
+++ Lv.1/3진법 뒤집기.js
@@ -11,3 +11,12 @@ function solution(n) {
     let number = n.toString(3).split('').reverse().join('')
     return parseInt(number, 3);
 }
+
+//Ivoryeee
+function solution(n) {
+    //n 3진수 변환 후 각 문자 reverse해 string이라는 변수로 joing시킴
+    const string = n.toString(3).split('').reverse().join('');
+    //string을 3진수로 변환
+    return parseInt(string, 3);
+
+}
