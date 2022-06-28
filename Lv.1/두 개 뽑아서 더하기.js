62396b25bb87d36b82b92e48813a80ec4d5099a4
Ivoryeee <105477246+Ivoryeee@users.noreply.github.com>
2022-06-29 01:51:19 +0900

    Update 두 개 뽑아서 더하기.js

--- Lv.1/두 개 뽑아서 더하기.js
+++ Lv.1/두 개 뽑아서 더하기.js
@@ -22,3 +22,16 @@ function solution(numbers) {
     // 중복 제거후 정렬
     return [...new Set(arr)].sort((a, b) => a - b);
 }
+
+//Ivoryeee
+function solution(numbers) {
+    let temp = new Set() //중복을 제거한 값의 집합 객체 Set 생성 
+    
+    for(let i = 0; i<numbers.length; i++) {
+        for(let j = i+1; j<numbers.length; j++) {
+            temp.add(numbers[i] + numbers[j]);
+        }
+    }
+    
+    return Array.from(temp).sort((a, b) => a - b);
+}
