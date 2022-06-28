d26ef32cbb337ec41103377a2caecd63350d0a8d
suzinxix <72126893+suzinxix@users.noreply.github.com>
2022-06-28 23:46:22 +0900

    Update 두 개 뽑아서 더하기.js

--- Lv.1/두 개 뽑아서 더하기.js
+++ Lv.1/두 개 뽑아서 더하기.js
@@ -9,3 +9,16 @@ function solution(numbers) {
     answer = Array.from(new Set(answer)).sort((a,b) => a-b);
     return answer;
 }
+
+
+// Dea
+function solution(numbers) {
+    let arr = [];
+    for(let i = 0; i < numbers.length - 1; i++){
+        for(let j = i+1; j < numbers.length; j++){
+            arr.push(numbers[i] + numbers[j]);
+        }
+    }
+    // 중복 제거후 정렬
+    return [...new Set(arr)].sort((a, b) => a - b);
+}
