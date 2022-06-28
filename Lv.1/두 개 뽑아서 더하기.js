038c2b6f52c0a13e63c7696945b65b26e6664a65
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-06-28 15:56:51 +0900

    Update 두 개 뽑아서 더하기.js

--- Lv.1/두 개 뽑아서 더하기.js
+++ Lv.1/두 개 뽑아서 더하기.js
@@ -1,1 +1,11 @@
-
+// Limbi
+function solution(numbers) {
+    let answer = [];
+    for (let i=0; i<numbers.length; i++) {
+        for (let j=i+1; j<numbers.length; j++) {
+            answer.push(numbers[i]+numbers[j]);
+        }
+    }
+    answer = Array.from(new Set(answer)).sort((a,b) => a-b);
+    return answer;
+}
