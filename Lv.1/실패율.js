5d4b8b51d865eef53d330c7457d638a55d7a2548
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-06-28 15:50:01 +0900

    Update 실패율.js

--- Lv.1/실패율.js
+++ Lv.1/실패율.js
@@ -1,1 +1,15 @@
+// Limbi
+function solution(N, stages) {
+    let answer = [];
+    let stage = [];
 
+    for (let i=0; i<N; i++) {   
+        let failCnt = stages.filter(n => n === i+1).length;
+        let playerCnt = stages.filter(n => n >= i+1).length;;    
+        
+        stage.push([i+1, failCnt/playerCnt]);
+    }
+    stage.sort((a,b) => b[1]-a[1]);
+    stage.map((i) => answer.push(i[0]));
+    return answer;
+}
