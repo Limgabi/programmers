d3e2acd38fc66257ce0e0623415d61641c044d80
suzinxix <72126893+suzinxix@users.noreply.github.com>
2022-07-06 04:02:11 +0900

    Update 2016년.js

--- Lv.1/2016년.js
+++ Lv.1/2016년.js
@@ -1,1 +1,39 @@
-
+// Dea
+function solution(a, b) {
+    let answer = '';
+    let day = 0;
+    const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    for(let i = 0; i < a-1; i++){
+        day += month[i];
+    }
+    
+    day += b;
+    
+    switch(day % 7){
+        case 0:
+            answer = "THU";
+            break;
+        case 1:
+            answer = "FRI";
+            break;
+        case 2:
+            answer = "SAT";
+            break;
+        case 3:
+            answer = "SUN";
+            break;
+        case 4:
+            answer = "MON";
+            break;
+        case 5:
+            answer = "TUE";
+            break;
+        case 6:
+            answer = "WED";
+            break;
+        default:
+            break;
+    }
+    
+    return answer;
+}
