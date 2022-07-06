1b692cf44f81995ec0aacd0e4da3081d8960ae4f
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-07-06 13:09:45 +0900

    Update 2016년.js

--- Lv.1/2016년.js
+++ Lv.1/2016년.js
@@ -37,3 +37,32 @@ function solution(a, b) {
     
     return answer;
 }
+
+// Limbi
+function solution(a, b) {
+    let answer = '';
+    let date = new Date(2016, a-1, b);
+    switch (date.getDay()) {
+        case 0:
+            answer = "SUN";
+            break;
+         case 1:
+            answer = "MON";
+            break;
+         case 2:
+            answer = "TUE";
+            break;
+         case 3:
+            answer = "WED";
+            break;
+         case 4:
+            answer = "THU";
+            break;
+         case 5:
+            answer = "FRI";
+            break;
+         case 6:
+            answer = "SAT";
+    }
+    return answer;
+}
