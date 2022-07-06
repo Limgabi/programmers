5424844bb2cd859e64c691c32c70ef7c9c2af030
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-07-06 13:17:00 +0900

    Update 최소직사각형.js

--- Lv.1/최소직사각형.js
+++ Lv.1/최소직사각형.js
@@ -11,3 +11,19 @@ function solution(sizes) {
     }
     return max * temp;
 }
+
+// Limbi
+function solution(sizes) {
+    let answer = 0;
+    let w = [];
+    let h = [];
+    for (let i=0; i<sizes.length; i++) {
+        if (sizes[i][0] > sizes[i][1]) {
+            [sizes[i][0], sizes[i][1]] = [sizes[i][1], sizes[i][0]];
+        }
+        w.push(sizes[i][0]);
+        h.push(sizes[i][1]);
+    }
+    answer = Math.max(...w) * Math.max(...h);
+    return answer;
+}
