5d41959905eba1de1ec71b9876c9b9973822027f
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-07-06 13:19:15 +0900

    Update 나머지가 1이 되는 수 찾기.js

--- Lv.1/나머지가 1이 되는 수 찾기.js
+++ Lv.1/나머지가 1이 되는 수 찾기.js
@@ -10,3 +10,15 @@ function solution(n) {
     }
     return answer;
 }
+
+// Limbi
+function solution(n) {
+    let answer = 0;
+    for (let i=2; i<n; i++) {
+        if (n % i === 1) {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
