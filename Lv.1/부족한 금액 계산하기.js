511061b8f27af6a6a70ff5c4ae7ebee8ae87656c
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-07-06 13:22:05 +0900

    Update 부족한 금액 계산하기.js

--- Lv.1/부족한 금액 계산하기.js
+++ Lv.1/부족한 금액 계산하기.js
@@ -6,3 +6,13 @@ function solution(price, money, count) {
     }
     return money > sum ? 0 : sum - money
 }
+
+// Limbi
+function solution(price, money, count) {
+    let answer = 0;
+    for (let i=0; i<count; i++) {
+        answer += price*(i+1);
+    }
+    money > answer ? answer = 0 : answer -= money;
+    return answer;
+}
