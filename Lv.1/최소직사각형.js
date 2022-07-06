28ce6e48ad47368b7b8217706906d4fc415272f2
Ivoryeee <105477246+Ivoryeee@users.noreply.github.com>
2022-07-06 14:17:27 +0900

    Update 최소직사각형.js

--- Lv.1/최소직사각형.js
+++ Lv.1/최소직사각형.js
@@ -27,3 +27,22 @@ function solution(sizes) {
     answer = Math.max(...w) * Math.max(...h);
     return answer;
 }
+
+//ivory
+function solution(sizes) {
+  let width = 0; //가로 중 가장 긴 값
+  let height = 0; //세로 중 가장 긴 값
+  
+  for(let len of sizes) {
+  //len[0]이 가로길이, len[1]이 세로길이 이다.
+    if(len[0] > len[1]){ // 가로가 더 긴 경우
+      if(width < len[0]) width = len[0]; //기존 width 새로운 가로 길이가 더 길 경우 width=len[0]
+      if(height < len[1]) height = len[1]; //기존 height 새로운 세로 길이가 더 길 경우 height=len[0]
+    }
+    else { // 세로가 더 긴 경우
+      if(width < len[1]) width = len[1]; //기존의 가로 중 가장 큰 값인 width보다 세로가 더 긴 경우
+      if(height < len[0]) height = len[0]; //기존의 세로 중 가장 큰 값인 height보다 세로가 더 긴 경우
+    }
+  };
+  return width * height;
+}
