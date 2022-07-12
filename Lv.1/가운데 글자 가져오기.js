d98ff7826b65579d9d8842657898d4bd0b88e3b5
Ivoryeee <105477246+Ivoryeee@users.noreply.github.com>
2022-07-13 00:17:51 +0900

    Update 가운데 글자 가져오기.js

--- Lv.1/가운데 글자 가져오기.js
+++ Lv.1/가운데 글자 가져오기.js
@@ -11,3 +11,16 @@ function solution(s) {
         
     return answer;
 }
+
+//ivoryeee
+
+function solution(s) {
+    let answer = '';
+    if (s.length % 2 == 0){
+        answer = s[s.length / 2 - 1] + s[s.length / 2];
+    }
+    else{
+        answer =  s[parseInt(s.length / 2)] ;
+    }   
+    return answer;
+}
