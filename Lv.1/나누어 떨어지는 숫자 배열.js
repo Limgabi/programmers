e03d25edadac02bac664496dced28062a11a316a
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-07-12 09:44:48 +0900

    Update 나누어 떨어지는 숫자 배열.js

--- Lv.1/나누어 떨어지는 숫자 배열.js
+++ Lv.1/나누어 떨어지는 숫자 배열.js
@@ -1,1 +1,12 @@
-
+// Limbi
+function solution(arr, divisor) {
+    let answer = [];
+    for (let i of arr) {
+        if (i % divisor === 0) answer.push(i);
+    }
+    if (answer.length === 0) {
+        answer.push(-1);
+    }
+    answer.sort((a,b) => a-b);
+    return answer;
+}
