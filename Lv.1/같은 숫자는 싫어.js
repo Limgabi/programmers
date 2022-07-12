f62c3df2e3062eade1978ec48aaa217e44ac6b9a
Ivoryeee <105477246+Ivoryeee@users.noreply.github.com>
2022-07-13 00:33:00 +0900

    Update 같은 숫자는 싫어.js

--- Lv.1/같은 숫자는 싫어.js
+++ Lv.1/같은 숫자는 싫어.js
@@ -5,3 +5,13 @@ function solution(arr)
     answer = arr.filter((e, idx) => e !== arr[idx+1]);
     return answer;
 }
+
+//ivory
+function solution(arr)
+{
+    var answer = [];
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i] !== arr[i+1]) answer.push(arr[i])
+    }
+    return answer;
+}
