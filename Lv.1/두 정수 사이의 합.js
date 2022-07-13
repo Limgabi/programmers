bf18c737dd61c1198f907384bc04ff2fa1a57101
suzinxix <72126893+suzinxix@users.noreply.github.com>
2022-07-13 10:50:56 +0900

    Update 두 정수 사이의 합.js

--- Lv.1/두 정수 사이의 합.js
+++ Lv.1/두 정수 사이의 합.js
@@ -29,3 +29,10 @@ function solution(a, b) {
     }
     return answer;
 }
+
+
+// Dea
+function solution(a, b) {
+    if(a >= b) return a*(a+1)/2 - b*(b-1)/2;
+    else return b*(b+1)/2 - a*(a-1)/2;
+}
