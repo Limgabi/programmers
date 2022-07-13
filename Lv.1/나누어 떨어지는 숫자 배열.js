e86147477b0a764d4ef7e71fd4aa8470d1632f33
suzinxix <72126893+suzinxix@users.noreply.github.com>
2022-07-13 10:51:27 +0900

    Update 나누어 떨어지는 숫자 배열.js

--- Lv.1/나누어 떨어지는 숫자 배열.js
+++ Lv.1/나누어 떨어지는 숫자 배열.js
@@ -25,3 +25,11 @@ function solution(arr, divisor) {
     answer.sort ((a, b) => a-b);   
     return answer;
 }
+
+
+// Dea
+function solution(arr, divisor) {
+    const answer = arr.filter(x => x % divisor === 0);
+
+    return answer.length !== 0 ? answer.sort((a, b) => a - b) : [-1];
+}
