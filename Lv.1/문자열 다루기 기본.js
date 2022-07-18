fb260975c7f8b24fc61ccfd5ed52435f2ab8f2cc
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-07-18 23:37:57 +0900

    Update 문자열 다루기 기본.js

--- Lv.1/문자열 다루기 기본.js
+++ Lv.1/문자열 다루기 기본.js
@@ -1,1 +1,6 @@
-
+// Limbi
+function solution(s) {
+    let answer = true;
+    answer = (s.length === 4 || s.length === 6) && (!isNaN(s)) && (!s.includes('e')) ? true : false;
+    return answer;
+}
