57c3f1d904bb731715777c752ccdcd6ef19b8db1
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-07-18 23:32:33 +0900

    Update 문자열 내 마음대로 정렬하기.js

--- Lv.1/문자열 내 마음대로 정렬하기.js
+++ Lv.1/문자열 내 마음대로 정렬하기.js
@@ -1,1 +1,6 @@
-
+// Limbi
+function solution(strings, n) {
+    let answer = [];
+    answer = strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
+    return answer;
+}
