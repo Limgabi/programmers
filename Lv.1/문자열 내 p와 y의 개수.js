a554a961e303f646fe1fc683c243182a4897af44
Limgabi <70315572+Limgabi@users.noreply.github.com>
2022-07-18 23:34:10 +0900

    Update 문자열 내 p와 y의 개수.js

--- Lv.1/문자열 내 p와 y의 개수.js
+++ Lv.1/문자열 내 p와 y의 개수.js
@@ -1,1 +1,11 @@
-
+// Limbi
+function solution(s){
+    let answer = true;
+    
+    let str = s.toLowerCase().split('');
+    let pCnt = str.filter(e => e === 'p').length;
+    let yCnt = str.filter(e => e === 'y').length;
+    
+    answer = pCnt === yCnt ? true : false;
+    return answer;
+}
