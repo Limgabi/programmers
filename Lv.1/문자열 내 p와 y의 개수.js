256638a5fcd134b202781908f8842e6f801d32fc
Ivoryeee <105477246+Ivoryeee@users.noreply.github.com>
2022-07-20 10:11:24 +0900

    Update 문자열 내 p와 y의 개수.js

--- Lv.1/문자열 내 p와 y의 개수.js
+++ Lv.1/문자열 내 p와 y의 개수.js
@@ -18,3 +18,15 @@ function solution(s){
 
     return p_count === y_count;
 }
+
+
+//ivoryeee
+function solution(s){
+    let str= s.toLowerCase();
+    let count = 0
+    for(let i = 0; i <s.length; i++){
+        if(str[i]==="p") count++;
+        else if(str[i]=== "y") count--
+    }
+    return count === 0 ? true : false
+}
