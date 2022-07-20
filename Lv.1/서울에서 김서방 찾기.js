7ab04d33a95794b7681b7c9e80e37729bf6062ba
Ivoryeee <105477246+Ivoryeee@users.noreply.github.com>
2022-07-20 10:17:56 +0900

    Update 서울에서 김서방 찾기.js

--- Lv.1/서울에서 김서방 찾기.js
+++ Lv.1/서울에서 김서방 찾기.js
@@ -11,3 +11,11 @@ function solution(seoul) {
 function solution(seoul) {
     return "김서방은 " + seoul.indexOf('Kim') + "에 있다";
 }
+
+
+//ivoryeee
+function solution(seoul) {
+    for(let i =0 ; i <seoul.length; i++){
+        if(seoul[i]==="Kim") return `김서방은 ${i}에 있다`
+    }
+}
